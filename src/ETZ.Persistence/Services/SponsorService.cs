3b4f0b33c2782bc633109f326ea4467839304b98
“KurtEmir” <emirkurt_2001@outlook.com>
2025-09-16 15:57:44 +0300

    chore: clean tracked build artifacts (obj/bin)

--- src/ETZ.Persistence/Services/SponsorService.cs
+++ src/ETZ.Persistence/Services/SponsorService.cs
@@ -1,5 +1,9 @@
 using ETZ.Persistence.Context;
 using Microsoft.Extensions.Logging;
+using ETZ.Domain.Entities;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
 namespace ETZ.Persistence.Services;
 
 public class SponsorService
@@ -12,5 +16,12 @@ public class SponsorService
     {
         _context = context;
         _logger = logger;
+    }
+
+
+    public async Task<List<Sponsor>> GetAllSponsors(Expression<Func<Sponsor, bool>> predicate)
+    {
+        var sponsors = await _context.Sponsors.AsTracking().Where(predicate).ToListAsync();
+        return sponsors;
     }
 }
